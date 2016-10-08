96633f26c71b4ff48147cd7df62888c7ddaa57dd
Denis SIMON SONEIRA <dsimonsoneira.ext@orange.com>
2016-10-08 08:40:32 +0200

    Init step 2

---
+++ src/main/java/co/simplon/exercise/web/controller/ApplicationController.java
@@ -0,0 +1,23 @@
+package co.simplon.exercise.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class ApplicationController {
+
+
+    @RequestMapping(name = "/params", method = RequestMethod.GET)
+    public @ResponseBody Map<String, String> params() {
+        Map<String, String> params = new HashMap<>();
+        params.put("key1", "value1");
+        params.put("key2", "value2");
+        return params;
+    }
+
+}
