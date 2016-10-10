579d75290d92d905547e63cb9bb8952e18d0baaa
Denis SIMON SONEIRA <dsimonsoneira.ext@orange.com>
2016-10-10 21:16:04 +0200

    RestController and CORS

--- src/main/java/co/simplon/exercise/web/controller/ApplicationController.java
+++ src/main/java/co/simplon/exercise/web/controller/ApplicationController.java
@@ -1,14 +1,9 @@
 package co.simplon.exercise.web.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Controller
+@RestController
+@CrossOrigin
 public class ApplicationController {
 
 
