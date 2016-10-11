df91de1ed646da5c8e3a0ff8ba27a773cfa0ae5e
Denis SIMON SONEIRA <dsimonsoneira.ext@orange.com>
2016-10-11 19:02:04 +0200

    Add empty constructor to Person

--- src/main/java/co/simplon/exercise/core/model/Person.java
+++ src/main/java/co/simplon/exercise/core/model/Person.java
@@ -4,6 +4,10 @@ public class Person {
 	private String name;
 	private String surname;
 
+	public Person() {
+		// TODO empty
+	}
+
 	public Person(String name, String surname) {
 		this.name = name;
 		this.surname = surname;
